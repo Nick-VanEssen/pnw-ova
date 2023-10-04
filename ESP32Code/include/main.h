14d05ca99cc420708426ee66d0dc34371453bfa4
doudar <anthonydoud97@gmail.com>
2023-10-04 09:38:05 -0500

    Added header files
    ### Added
    - Added Header Files
    - Added CHANGELOG.md
    - Added gitignore to root of project.
    
    ### Changed
    - Moved some static assigns to header files.
    - Removed "progmem" as it's not required with ESP32.
    - Added a fixed version of the platform in platformio.ini.

---
+++ ESP32Code/include/main.h
@@ -0,0 +1,8 @@
+#pragma once //prevent file from being included more than once
+
+// Replace with your network credentials
+const char* ssid = "Kenwood Boys";
+const char* password = "pnwbaseball";
+
+const char* PARAM_INPUT_1 = "output";
+const char* PARAM_INPUT_2 = "state";
---
+++ ESP32Code/src/main.cpp
@@ -0,0 +1,58 @@
+// Import required libraries
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+#include "main.h"
+#include "builtinPage.h"
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage1;
+    String inputMessage2;
+    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+    if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
+      inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+      digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
+    }
+    else {
+      inputMessage1 = "No message sent";
+      inputMessage2 = "No message sent";
+    }
+    Serial.print("GPIO: ");
+    Serial.print(inputMessage1);
+    Serial.print(" - Set to: ");
+    Serial.println(inputMessage2);
+    request->send(200, "text/plain", "OK");
+  });
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+
+}
