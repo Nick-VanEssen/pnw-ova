b0a3d43d26da0401ce17c1fe6423faa75b7bb19f
AndrewPetsche <apetsch@pnw.edu>
2023-10-25 18:10:36 -0500

    work on reading data off mic

--- src/acc.cpp
+++ src/acc.cpp
@@ -1,33 +1,33 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL345_U.h>
+// #include <Wire.h>
+// #include <Adafruit_Sensor.h>
+// #include <Adafruit_ADXL345_U.h>
   
-/*Initialize an instance of Adafruit_ADXL345_Unified with a unique id*/
+// /*Initialize an instance of Adafruit_ADXL345_Unified with a unique id*/
 
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+// Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
  
-void setup() {
+// void setup() {
  
-  Serial.begin(115200);
-  Serial.println("");
-  Serial.println("Small example to read from ADXL345 accelerometer");
-  accel.begin();
+//   Serial.begin(115200);
+//   Serial.println("");
+//   Serial.println("Small example to read from ADXL345 accelerometer");
+//   accel.begin();
  
-}
+// }
  
-void loop() {
- Serial.println("");
-  /*Read from ADXL345 accelerometer*/
-  sensors_event_t event; 
-  accel.getEvent(&event);
-  // Serial.println("Printing XYZ: ");
-  // Serial.println("");
-  // /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
-  // /*Take a one second break*/
-  delay(100);
+// void loop() {
+//  Serial.println("");
+//   /*Read from ADXL345 accelerometer*/
+//   sensors_event_t event; 
+//   accel.getEvent(&event);
+//   // Serial.println("Printing XYZ: ");
+//   // Serial.println("");
+//   // /* Display the results (acceleration is measured in m/s^2) */
+//   Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+//   Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+//   Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+//   // /*Take a one second break*/
+//   delay(100);
 
  
-}
+// }
