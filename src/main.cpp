2b397232a73ca9536501e4da3393eb7ab0e44da4
Matt VanEssen <mattnick10@hotmail.com>
2023-11-08 20:46:49 -0600

    Code fix and cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -29,27 +29,23 @@ String processor(const String &var)
   return String();
 }
 
-//--------------Email Defines-----------------
+/********** Email Notifications Defines **********/
 
-// not sure if needed
-// #define WIFI_SSID "treestand"
-// #define WIFI_PASSWORD "iceman25"
-
-/** The smtp host name e.g. smtp.gmail.com for GMail or smtp.office365.com for Outlook or smtp.mail.yahoo.com */
+// The smtp host name e.g. smtp.gmail.com for GMail or smtp.office365.com for Outlook or smtp.mail.yahoo.com
 #define SMTP_HOST "smtp.gmail.com"
 #define SMTP_PORT 465
 
-/* The sign in credentials */
+// The sign in credentials
 #define AUTHOR_EMAIL "open.vibration.analysis@gmail.com"
 #define AUTHOR_PASSWORD "uiea whvy jzve qplh"
 
-/* Recipient's email*/
+// Recipient's email
 #define RECIPIENT_EMAIL "mvanessen15@gmail.com"
 
-/* Declare the global used SMTPSession object for SMTP transport */
+// Declare the global used SMTPSession object for SMTP transport
 SMTPSession smtp;
 
-/* Callback function to get the Email sending status */
+// Callback function to get the Email sending status
 void smtpCallback(SMTP_Status status);
 
 void setup()
@@ -61,7 +57,8 @@ void setup()
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
 
-  // WiFi Manager setup
+  /***************** WiFi Manager *****************/
+
   bool res;
   wm.setConfigPortalBlocking(false);                  // don't wait for user wifi setup
   res = wm.autoConnect("OVA WiFi Setup", "password"); // ssid and password for access point
@@ -72,53 +69,51 @@ void setup()
   Serial.println("Connection Successful!");
   setupPDM();
 
-  // ----------------Email Setup-----------------------------
+  /***************** Email Notifications *****************/
 
-  /*  Set the network reconnection option */
+  //Set the network reconnection option
   MailClient.networkReconnect(true);
 
-  /** Enable the debug via Serial port
-   * 0 for no debugging
-   * 1 for basic level debugging
-   *
-   * Debug port can be changed via ESP_MAIL_DEFAULT_DEBUG_PORT in ESP_Mail_FS.h
-   */
+  // Enable the debug via Serial port
+  // 0 for no debugging
+  // 1 for basic level debugging
+  // Debug port can be changed via ESP_MAIL_DEFAULT_DEBUG_PORT in ESP_Mail_FS.h
   smtp.debug(1);
 
-  /* Set the callback function to get the sending results */
+  // Set the callback function to get the sending results
   smtp.callback(smtpCallback);
 
-  /* Declare the Session_Config for user defined session credentials */
+  // Declare the Session_Config for user defined session credentials
   Session_Config config;
 
-  /* Set the session config */
+  // Set the session config
   config.server.host_name = SMTP_HOST;
   config.server.port = SMTP_PORT;
   config.login.email = AUTHOR_EMAIL;
   config.login.password = AUTHOR_PASSWORD;
   config.login.user_domain = "";
 
-  /*
-  Set the NTP config time
-  For times east of the Prime Meridian use 0-12
-  For times west of the Prime Meridian add 12 to the offset.
-  Ex. American/Denver GMT would be -6. 6 + 12 = 18
-  See https://en.wikipedia.org/wiki/Time_zone for a list of the GMT/UTC timezone offsets
-  */
+  
+  // Set the NTP config time
+  // For times east of the Prime Meridian use 0-12
+  // For times west of the Prime Meridian add 12 to the offset.
+  // Ex. American/Denver GMT would be -6. 6 + 12 = 18
+  // See https://en.wikipedia.org/wiki/Time_zone for a list of the GMT/UTC timezone offsets
+  
   config.time.ntp_server = F("pool.ntp.org,time.nist.gov");
-  config.time.gmt_offset = 3;
+  config.time.gmt_offset = 18; //cst time zone
   config.time.day_light_offset = 0;
 
-  /* Declare the message class */
+  // Declare the message class
   SMTP_Message message;
 
-  /* Set the message headers */
+  // Set the message headers
   message.sender.name = F("Open Vibration Analysis");
   message.sender.email = AUTHOR_EMAIL;
   message.subject = F("OVA Test Email");
   message.addRecipient(F("Test"), RECIPIENT_EMAIL);
 
-  /*Send HTML message*/
+  // Send HTML message
   /*String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Hello World!</h1><p>- Sent from ESP board</p></div>";
   message.html.content = htmlMsg.c_str();
   message.html.content = htmlMsg.c_str();
@@ -126,7 +121,7 @@ void setup()
   message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;*/
 
   // Send raw text message
-  String textMsg = "Hello World! - Sent from ESP board";
+  String textMsg = "Hello World! - Sent from the OVA ESP32";
   message.text.content = textMsg.c_str();
   message.text.charSet = "us-ascii";
   message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
@@ -134,7 +129,7 @@ void setup()
   message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_low;
   message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
 
-  /* Connect to the server */
+  // Connect to the server
   if (!smtp.connect(&config))
   {
     ESP_MAIL_PRINTF("Connection error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
@@ -153,27 +148,20 @@ void setup()
       Serial.println("\nConnected with no Auth.");
   }
 
-  /* Start sending Email and close the session */
+  // Start sending Email and close the session
   if (!MailClient.sendMail(&smtp, &message))
     ESP_MAIL_PRINTF("Error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
 }
 
-void loop()
-{
-  digitalWrite(ledPin, ledState);
-  loopPDM();
-  wm.process();
-}
+/********** Does this belong in or after void loop()? Not sure, putting it before it for now **********/
 
-//-----------Rest of Email Setup. Does this belong in void loop()??-----------------------
-
-/* Callback function to get the Email sending status */
+// Callback function to get the Email sending status
 void smtpCallback(SMTP_Status status)
 {
-  /* Print the current status */
+  // Print the current status
   Serial.println(status.info());
 
-  /* Print the sending result */
+  // Print the sending result
   if (status.success())
   {
     // ESP_MAIL_PRINTF used in the examples is for format printing via debug Serial port
@@ -187,7 +175,7 @@ void smtpCallback(SMTP_Status status)
 
     for (size_t i = 0; i < smtp.sendingResult.size(); i++)
     {
-      /* Get the result item */
+      // Get the result item
       SMTP_Result result = smtp.sendingResult.getItem(i);
 
       // In case, ESP32, ESP8266 and SAMD device, the timestamp get from result.timestamp should be valid if
@@ -203,7 +191,17 @@ void smtpCallback(SMTP_Status status)
     }
     Serial.println("----------------\n");
 
-    // You need to clear sending result as the memory usage will grow up.
+    // You need to clear sending result as the memory usage will grow.
     smtp.sendingResult.clear();
   }
+}
+
+void loop()
+{
+  digitalWrite(ledPin, ledState);
+  
+  // commented out to clean terminal output
+  // loopPDM();
+
+  wm.process();
 }
